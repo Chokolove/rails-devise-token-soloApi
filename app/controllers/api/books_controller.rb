5d19ace17f7db33b987e8ca92ccf344928caa3ae
chokolove <carlosll2493@gmail.com>
2021-01-12 09:29:46 -0500

    Add model Book and controllers

---
+++ app/controllers/api/books_controller.rb
@@ -0,0 +1,46 @@
+class Api::BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+
+  def index
+    render json: Book.all
+  end
+
+  def show
+    render json: @book
+  end
+  
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book , status: :created
+    else
+      render json: {}
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: {}
+    end
+  end
+
+  def destroy
+    @book.destroy
+    render json: {} , status: :no_content
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def book_params
+    params.require(:book).permit(:title, :summary)
+  end
+end
