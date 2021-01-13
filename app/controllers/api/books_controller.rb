f064548819ca00dc1dd58d805823d30fec78a007
chokolove <carlosll2493@gmail.com>
2021-01-12 21:30:02 -0500

    Add devise_token_auth

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,5 +1,6 @@
 class Api::BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   def index
     render json: Book.all
